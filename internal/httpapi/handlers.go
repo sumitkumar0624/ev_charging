00ef2d2f9d6e38d628e333b861c608dda054c1bc
sumitkumar0624 <47843815+sumitkumar0624@users.noreply.github.com>
2026-09-17 13:53:01 +0530

    Update handlers.go

--- internal/httpapi/handlers.go
+++ internal/httpapi/handlers.go
@@ -9,62 +9,62 @@ import (
 
 func (h *Handler) registerDriver(w http.ResponseWriter, r *http.Request) {
 	var input models.RegisterDriverInput
-	if !decode(w, r, &input) {
+	if !h.decode(w, r, &input) {
 		return
 	}
 	driver, err := h.service.RegisterDriver(r.Context(), input)
-	respond(w, driver, err, http.StatusCreated)
+	h.respond(w, r, driver, err, http.StatusCreated)
 }
 
 func (h *Handler) registerStation(w http.ResponseWriter, r *http.Request) {
 	var input models.RegisterStationInput
-	if !decode(w, r, &input) {
+	if !h.decode(w, r, &input) {
 		return
 	}
 	station, err := h.service.RegisterStation(r.Context(), input)
-	respond(w, station, err, http.StatusCreated)
+	h.respond(w, r, station, err, http.StatusCreated)
 }
 
 func (h *Handler) startSession(w http.ResponseWriter, r *http.Request) {
 	var input models.StartSessionInput
-	if !decode(w, r, &input) {
+	if !h.decode(w, r, &input) {
 		return
 	}
 	session, err := h.service.StartSession(r.Context(), input)
-	respond(w, session, err, http.StatusCreated)
+	h.respond(w, r, session, err, http.StatusCreated)
 }
 
 func (h *Handler) endSession(w http.ResponseWriter, r *http.Request) {
 	var body models.EndSessionInput
-	if !decode(w, r, &body) {
+	if !h.decode(w, r, &body) {
 		return
 	}
 	session, err := h.service.EndSession(r.Context(), chi.URLParam(r, "sessionID"), body.EnergyDeliveredKWh)
-	respond(w, session, err, http.StatusOK)
+	h.respond(w, r, session, err, http.StatusOK)
 }
 
 func (h *Handler) setConnectorStatus(w http.ResponseWriter, r *http.Request) {
 	var body models.SetConnectorStatusInput
-	if !decode(w, r, &body) {
+	if !h.decode(w, r, &body) {
 		return
 	}
 	connector, err := h.service.SetConnectorStatus(r.Context(), chi.URLParam(r, "connectorID"), body.Status)
-	respond(w, connector, err, http.StatusOK)
+	h.respond(w, r, connector, err, http.StatusOK)
 }
 
 func (h *Handler) createPromo(w http.ResponseWriter, r *http.Request) {
 	var input models.CreatePromoInput
-	if !decode(w, r, &input) {
+	if !h.decode(w, r, &input) {
 		return
 	}
 	promo, err := h.service.CreatePromo(r.Context(), input)
-	respond(w, promo, err, http.StatusCreated)
+	h.respond(w, r, promo, err, http.StatusCreated)
 }
 
 func (h *Handler) deletePromo(w http.ResponseWriter, r *http.Request) {
 	err := h.service.DeletePromo(r.Context(), chi.URLParam(r, "code"))
 	if err != nil {
-		respond(w, nil, err, 0)
+		h.respond(w, r, nil, err, 0)
 		return
 	}
 	w.WriteHeader(http.StatusNoContent)
@@ -72,10 +72,10 @@ func (h *Handler) deletePromo(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handler) driverSessions(w http.ResponseWriter, r *http.Request) {
 	sessions, err := h.service.DriverSessions(r.Context(), chi.URLParam(r, "driverID"))
-	respond(w, sessions, err, http.StatusOK)
+	h.respond(w, r, sessions, err, http.StatusOK)
 }
 
 func (h *Handler) stationSessions(w http.ResponseWriter, r *http.Request) {
 	sessions, err := h.service.StationSessions(r.Context(), chi.URLParam(r, "stationID"))
-	respond(w, sessions, err, http.StatusOK)
+	h.respond(w, r, sessions, err, http.StatusOK)
 }
